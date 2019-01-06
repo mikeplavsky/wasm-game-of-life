164b13aa0d0ab0ebb9fd89a3a10808f182cb9948
mikeplavsky <mike.plavsky@quest.com>
2019-01-07 02:12:50 +0300

    started universe

--- src/lib.rs
+++ src/lib.rs
@@ -21,5 +21,38 @@ cfg_if! {
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Cell {
     Dead = 0,
-    Alive = 1
-};
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: usize,
+    height: usize,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    pub fn new() -> Universe {
+        Universe {
+            width: 4,
+            height: 4,
+            cells: vec![],
+        }
+    }
+
+    pub fn get_index(&self, row: usize, column: usize) -> usize {
+        row * self.width + column
+    }
+
+    pub fn live_neighbor_count(&self, row: usize, column: usize) -> usize {
+        let mut count = 0;
+        for d_r in vec![self.width - 1, 0, 1] {
+            for d_c in vec![self.height - 1, 0, 1] {
+
+                if d_c == 0 && d_r == 0 { continue; }
+
+            }
+        }
+        count
+    }
+}
--- tests/web.rs
+++ tests/web.rs
@@ -5,9 +5,16 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
+extern crate wasm_game_of_life;
+use wasm_game_of_life::*;
+
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn test_unverse_index() {
+    let u = Universe::new();
+
+    assert_eq!(0, u.get_index(0, 0));
+    assert_eq!(1, u.get_index(0, 1));
+    assert_eq!(5, u.get_index(1, 1));
 }
